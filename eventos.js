6c7537c4844079a0a99569574323f20aaff7dfaf
Luis <luismtorresrequena@gmail.com>
2020-09-12 09:09:46 -0300

    Mi Portafolio

--- eventos.js
+++ eventos.js
@@ -35,9 +35,9 @@ window.addEventListener('click', function(e){
 var perfil = document.getElementById('perfil');
 var animacion = document.getElementById('listaPerfil');
 
-if (screen.width < 1366 && screen.height < 768) {
+if (screen.width < 1366 && screen.height < 768){
   perfil.classList.add('perfil2');
-  animacion.classList.add('animacion2')
+  animacion.classList.add('animacion2');
 } else {
   perfil.classList.remove('perfil2');
   animacion.classList.remove('animacion2');
